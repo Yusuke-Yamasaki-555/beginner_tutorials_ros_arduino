00af9c535f5954bd4d3b2388bab461873a2058d0
Yusuke Yamasaki <yuu5364yuu@gmail.com>
2022-05-27 21:21:47 +0900

    Update

--- src/talker_arduino.cpp
+++ src/talker_arduino.cpp
@@ -8,12 +8,13 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "talker_arduino");
     ros::NodeHandle nh;
     ros::Publisher pub_led = nh.advertise<std_msgs::Int32>("led", 10);
-    ros::Rate loop_rate(1);
+    ros::Rate loop_rate(0.1);
 
-    led_msg.data = 1
+    led_msg.data = 1;
 
     while(ros::ok())
     {
+        ROS_INFO("I heard: [%d]", led_msg.data);
         pub_led.publish(led_msg);
         ros::spinOnce();
         loop_rate.sleep();
