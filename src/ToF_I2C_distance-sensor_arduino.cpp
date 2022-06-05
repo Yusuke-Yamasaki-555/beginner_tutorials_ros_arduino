00d6b901c3c3c8224136adbc9fce93b44750b4df
Yusuke Yamasaki <yuu5364yuu@gmail.com>
2022-06-05 12:36:58 +0900

    Create ToF_I2C_distance-sensor_arduino.cpp

---
+++ src/ToF_I2C_distance-sensor_arduino.cpp
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+void chatterCallback(const std_msgs::Int32 &distance_1_msg)
+{
+  ROS_INFO("Distance: %d mm", distance_1_msg.data);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "listener_distance_1");
+
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("distance_1", 45, chatterCallback);
+
+  ros::spin();
+
+  return 0;
+}
