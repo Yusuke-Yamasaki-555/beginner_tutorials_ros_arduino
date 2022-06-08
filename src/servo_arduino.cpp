5ac8ac3219cf923ae703b80676bcd73631efb2a1
Yusuke Yamasaki <yuu5364yuu@gmail.com>
2022-06-09 00:08:53 +0900

    Update servo_arduino.cpp (Unverified)

--- src/servo_arduino.cpp
+++ src/servo_arduino.cpp
@@ -5,40 +5,35 @@
 std_msgs::Int32 msg;
 std_msgs::Int32 hoge;
 
-void servoCallback(const std_msgs::Int32& msg)
+void servoCallback(const std_msgs::Int32 &msg)
 {
     printf("Now servo: [%d]\n", msg.data);
+
+    msg.data = rand() % 180 + 1;
+
+    ROS_INFO("PubData [%d]", msg.data);
+
+    servo_pub.publish(msg);
+
+    loop_rate.sleep();
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "servo_node");
-
     ros::NodeHandle n;
 
-    ros::Publisher servo_pub = n.advertise<std_msgs::Int32>("dir_ang", 1000);
+    ros::Publisher servo_pub = n.advertise<std_msgs::Int32>("dir_ang", 10);
+    ros::Subscriber servo_sub = n.subscribe("now_ang", 10, servoCallback);
 
-    ros::Rate loop_rate(100);
+    ros::Rate loop_rate(16000000);
 
-    ros::Subscriber servo_sub = n.subscribe("now_ang", 1000, servoCallback);
+    msg.data = 0;
+    ROS_INFO("Set Servo\n");
+    servo_pub.publish(msg);
+    loop_rate.sleep();
 
-    while(ros::ok()){
-        printf("hoge");
-        ros::spinOnce();
-        sleep(1000);
-    }
-
-    while(ros::ok()){
-
-        msg.data = rand() % 180 + 1;
-
-        ROS_INFO("PubData [%d]", msg.data);
-
-        servo_pub.publish(msg);
-
-        loop_rate.sleep();
-    }
-
-
+    ros::spin();
+    
     return 0;
 }
