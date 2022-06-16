3f4e4fa5ef1ac47cda4ea573e21431b9173d0e5b
Yusuke Yamasaki <yuu5364yuu@gmail.com>
2022-06-16 17:20:01 +0900

    Finish color_sensor_arduino

--- src/color_sensor_arduino.cpp
+++ src/color_sensor_arduino.cpp
@@ -3,23 +3,20 @@
     Copyright (C) 2022 Yusuke Yamasaki. All Rights Reserved.
 */
 
-// 独自message_fileを作成して、RGBの値を別々に送る。
-// default 
-// message_name : ColorRGB
-// message_data : Int32 red , Int32 green , Int32 blue
-
 #include "ros/ros.h"
 #include "std_msgs/Int32.h"
-// #include "message/ColorRGB" こういう書き方で良いかは知らん
+#include "beginner_tutorials/Data4.h"
 
-// SUBSCRIBER
-// void colorCallback(const <ココに型定義>& sub_color_msg){}
-/*
+void colorCallback(const beginner_tutorials::Data4 &sub_color_msg)
+{
+    ROS_INFO("COLOR_DATA : R[ %u ], G[ %u ], B[ %u ], IR[ %u ]", sub_color_msg.data1, sub_color_msg.data2, sub_color_msg.data3, sub_color_msg.data4);
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "color_sensor");
 
-    ros NodeHandle n;
+    ros::NodeHandle n;
 
     ros::Subscriber sub_color = n.subscribe("color_msg", 10, colorCallback);
 
@@ -27,4 +24,3 @@ int main(int argc, char **argv)
 
     return 0;
 }
-*/
